2e544e8d1a27631425ccac399a0482cc5c35a520
Alexej Nenastev <alexey.ne.bcs@mail.ru>
2019-02-01 16:07:47 +0300

    Initial Commit

---
+++ ModuleCore/ModuleCore.h
@@ -0,0 +1,19 @@
+//
+//  ModuleCore.h
+//  ModuleCore
+//
+//  Created by alexej_ne on 01/02/2019.
+//  Copyright © 2019 BCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModuleCore.
+FOUNDATION_EXPORT double ModuleCoreVersionNumber;
+
+//! Project version string for ModuleCore.
+FOUNDATION_EXPORT const unsigned char ModuleCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleCore/PublicHeader.h>
+
+
