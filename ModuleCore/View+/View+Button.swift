ff59fd3928f56ac9ff5c4aeb8d5fe8a3f6ce2682
vaspondm <ponomarev.bcs@gmail.com>
2019-02-13 14:03:43 +0300

    checkout commit

--- ModuleCore/View+/View+Button.swift
+++ ModuleCore/View+/View+Button.swift
@@ -11,9 +11,22 @@ import RxSwift
 import RxCocoa
 
 public extension SceneView {
+    var vm: Reactor {
+        guard let vm = reactor else {  fatalError()  }
+        return vm
+    }
+
     func enable(button: UIButton, when stateMap: @escaping (Reactor.State) -> Bool) {
         rctr.state.map(stateMap)
             .bind(to: button.rx.isEnabled)
             .disposed(by: disposeBag)
     }
+
+    func fire(_ action: Reactor.Action, onTap button: UIButton, disposeBag: DisposeBag? = nil) {
+        button.rx.tap
+            .map { action }
+            .bind(to: vm.action)
+            .disposed(by: disposeBag ?? self.disposeBag)
+    }
+
 }
