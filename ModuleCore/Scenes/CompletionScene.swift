b88cb81d248e006e97349c292d145730d268f532
vaspondm <ponomarev.bcs@gmail.com>
2019-05-20 18:57:57 +0300

    completion scene исправления

--- ModuleCore/Scenes/CompletionScene.swift
+++ ModuleCore/Scenes/CompletionScene.swift
@@ -12,19 +12,19 @@ public typealias Next = Void
 public typealias CustomPresentationFunc = (Presenter) -> Void
 
 open class CompletionScene<T> {
-    open let scene: Scene
-    open let completion: Observable<T>
+    public let scene: Scene
+    public let completion: Observable<T>
     
     // Сцена может предоставлять кастомную функцию для ее презентации
-    open let customModalPresentatioin: CustomPresentationFunc?
+    public let customModalPresentatioin: CustomPresentationFunc?
     
-    open init(_ scene: Scene, _ reactor: CompletableReactor<T>, customPresentation: CustomPresentationFunc? = nil) {
+    public init(_ scene: Scene, _ reactor: CompletableReactor<T>, customPresentation: CustomPresentationFunc? = nil) {
         self.scene = scene
         self.completion = reactor.onComplete
         self.customModalPresentatioin = customPresentation
     }
     
-    open init(_ scene: Scene, completion: Observable<T>, customPresentation: CustomPresentationFunc? = nil) {
+    public init(_ scene: Scene, completion: Observable<T>, customPresentation: CustomPresentationFunc? = nil) {
         self.scene = scene
         self.completion = completion
         self.customModalPresentatioin = customPresentation
