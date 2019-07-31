36994a06d4a8b834d418dfe15a2adfc6e5f878ed
Alexej Nenastev <alexey.ne.bcs@mail.ru>
2019-07-31 17:19:01 +0300

    MB-0000 helperss

---
+++ ModuleCore/TableScene/SectionModelType+.swift
@@ -0,0 +1,40 @@
+//
+//  AnimatableSectionModel+.swift
+//  ModuleCore
+//
+//  Created by alexej_ne on 30.07.2019.
+//  Copyright © 2019 BCS. All rights reserved.
+//
+
+
+import RxDataSources
+
+public extension AnimatableSectionModel {
+    mutating func addItems(items: [Item]) {
+        self.items += items
+    }
+}
+
+public extension Array where Element: SectionModelType {
+    
+    subscript(_ ip: IndexPath) -> Element.Item {
+        get { return self[ip.section].items[ip.row] }
+    }
+    
+}
+
+public extension AnimatableSectionModel {
+    func merge(new array: [Self], to old: inout [Self]) {
+        if let last = old.last, let newFirst = old.first, last.identity == newFirst.identity   {
+            let lastIndex = old.count - 1
+            old[lastIndex].addItems(items: newFirst.items)
+            var array = array
+            array.removeFirst()
+            old.append(contentsOf: array)
+        } else {
+            old.append(contentsOf: array)
+        }
+    }
+}
+
+ 
