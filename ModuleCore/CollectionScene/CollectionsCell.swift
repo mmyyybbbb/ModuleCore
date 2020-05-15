b33ab20fa4a8d1f353b251769e732fa5584add73
Alexey Nenastev <a-nenastev@mail.ru>
2020-05-15 13:02:55 +0300

    Codable для CollectionCell

--- ModuleCore/CollectionScene/CollectionsCell.swift
+++ ModuleCore/CollectionScene/CollectionsCell.swift
@@ -12,11 +12,45 @@ public enum CollectionCell<T> {
     case showMore(ShowMoreCellData)
 }
 
+extension String : Error {}
+
+extension CollectionCell: Codable where T: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case item
+        case showMore
+    }
+     
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        if let value = try? values.decode(T.self, forKey: .item) {
+            self = .item(value)
+            return
+        } else if let value = try? values.decode(ShowMoreCellData.self, forKey: .showMore) {
+            self = .showMore(value)
+            return
+        }
+        
+        throw  "Whoops! \(dump(values))"
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .item(let data):
+            try container.encode(data, forKey: .item)
+        case .showMore(let showMore):
+            try container.encode(showMore, forKey: .showMore)
+        }
+    }
+}
+
+
 /// Данные в ячейке ShowMore
-public struct ShowMoreCellData {
+public struct ShowMoreCellData: Codable {
     public let title: String
     
     public init(title: String) {
         self.title = title
     }
 }
+
