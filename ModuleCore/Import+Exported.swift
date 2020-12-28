d7e0b6543dadfbfa311797ac4f10a46b5a25a279
Nikolay Leontev <bently_93@mail.ru>
2020-12-28 09:38:06 +0300

    Swift package manager

---
+++ ModuleCore/Import+Exported.swift
@@ -0,0 +1,10 @@
+//
+//  Import+Exported.swift
+//  ModuleCore
+//
+//  Created by n.leontev on 25.12.2020.
+//  Copyright © 2020 BCS. All rights reserved.
+//
+
+@_exported import Foundation
+@_exported import UIKit
---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "ModuleCore",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "ModuleCore", targets: ["ModuleCore"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMinor(from: "5.1.0")),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources", .upToNextMinor(from: "4.0.1")),
+        .package(url: "https://github.com/ReactorKit/ReactorKit", .upToNextMajor(from: "2.0.1")),
+        .package(url: "https://github.com/devxoul/RxViewController", .upToNextMinor(from: "1.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "ModuleCore",
+            dependencies: ["RxCocoa",
+                           "RxDataSources",
+                           "ReactorKit",
+                           "RxViewController"],
+            path: "ModuleCore"
+        )
+    ]
+)
